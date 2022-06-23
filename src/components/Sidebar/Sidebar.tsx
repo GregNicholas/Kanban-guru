a2d726e57cc58a273f92bf818e047ab745073b59
Greg Schoenberg <gregschoenberg@gmail.com>
2022-06-22 23:39:35 -0700

    sidebar select dark and light hover states

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -31,7 +31,7 @@ const Sidebar = (
         <div className="mb-11">
             <DarkModeSelect isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
             <div
-                className="py-3.5 ml-8 text-m-gray font-bold cursor-pointer"
+                className="py-3.5 pl-8 mr-6 text-m-gray font-bold cursor-pointer transition duration-300 rounded-r-3xl hover:text-main-purple hover:fill-main-purple hover:bg-main-purple hover:bg-opacity-10 dark:hover:bg-white"
                 onClick={() => setShowSidebar(prev => !prev)}
             >
                 <img className="h-5 mr-4 inline cursor-pointer" src={`${process.env.PUBLIC_URL}/assets/icon-hide-sidebar.svg`}
