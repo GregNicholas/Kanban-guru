b71e64d92d7ab9eda16b908e3c75875ffda69a68
Greg Schoenberg <gregschoenberg@gmail.com>
2022-07-07 20:02:54 -0700

    custom styling button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,17 +3,19 @@ import React from 'react'
 type ButtonProps = {
     type?: "button" | "submit";
     text: string;
-    onClick: (e: any) => void;
+    onClick?: (e: any) => void;
     primary?: boolean;
     customStyle?: string | boolean;
+    widthFull?: boolean;
 }
 
-const Button = ({ type="button", text, onClick, primary=true, customStyle=false }:ButtonProps) => {
+const Button = ({ type="button", text, onClick, primary=true, customStyle=false, widthFull=true }:ButtonProps) => {
   const color = customStyle ? customStyle
                         : primary ? "text-white bg-main-purple hover:bg-main-purple-hover active:bg-white active:border active:text-main-purple"
                         : "text-main-purple bg-main-purple-hover dark:bg-white"
+  const width = widthFull ? "w-full" : null
   return (
-    <button type={type} className={`${color} font-bold rounded-3xl px-4 py-3 min-w-[162px]`}
+    <button type={type} className={`${color} ${width} font-bold rounded-3xl px-4 py-3 min-w-[162px]`}
       onClick={onClick}
     >
         {text}
